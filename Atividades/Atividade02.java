1cd8a8155a60fa890cec1c255c6b36f98b2f63f1
Tiago Ramos Aguiar <126036647+tiagoramosaguiar@users.noreply.github.com>
2023-03-14 00:57:19 -0300

    Update Atividade02.java

--- Atividades/Atividade02.java
+++ Atividades/Atividade02.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 
 public class Atividade02 {
     public static void main(String[] args) {
-        Integer i;
-        String textoInvertido, texto;
         Scanner scan = new Scanner(System.in);
+        String textoInvertido, texto;
+        Integer i;
+        
         System.out.print("Insira algo: ");
         texto = scan.nextLine();
         textoInvertido="";
+        
         for (i = texto.length()-1; i >= 0; i--) {
             textoInvertido = textoInvertido + texto.charAt(i);
         }
+        
         System.out.println("↓");
         System.out.print("Inversão: " + textoInvertido);
     }
